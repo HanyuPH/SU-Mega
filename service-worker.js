a6f019fa1cce62dc6a96e36288171a0da18eb967
HanyuPH <pedrohenriquefq@hotmail.com>
2026-08-03 00:06:32 -0300

    Carregar correção de sincronização na Beta v25

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "su-mega-c2-beta-v24";
+const CACHE_NAME = "su-mega-c2-beta-v25";
 const APP_ASSETS = [
   "./",
   "./index.html",
@@ -60,9 +60,9 @@ async function officialResultsWithCloud(request) {
     response = await cache.match(request);
   }
 
-  const loader = "\n;import('./beta-banner.js?v=24')"
+  const loader = "\n;import('./beta-banner.js?v=25')"
     + ".then(()=>import('./beta-layout-review.js?v=10'))"
-    + ".then(()=>import('./realtime-cloud.js?v=1'))"
+    + ".then(()=>import('./realtime-cloud.js?v=2'))"
     + ".then(()=>import('./account-panel.js?v=2'))"
     + ".then(()=>import('./ecosystem-ui.js?v=7'))"
     + ".then(()=>import('./prize-analysis.js?v=2'))"
