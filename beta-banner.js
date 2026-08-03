4c6f7be6ad9c0336775f76479d5d3f00ed0569d6
HanyuPH <pedrohenriquefq@hotmail.com>
2026-08-02 21:04:42 -0300

    Identificar SU Mega Beta v21 e carregar ponte de sincronização

--- beta-banner.js
+++ beta-banner.js
@@ -1,6 +1,6 @@
 (() => {
   "use strict";
-  const BUILD = "v20";
+  const BUILD = "v21";
 
   function init() {
     let bar = document.getElementById("su-beta-banner");
@@ -19,4 +19,8 @@
   } else {
     init();
   }
+
+  import("./sync-bridge.js?v=1").catch(error => {
+    console.error("SU Mega ponte de sincronização:", error);
+  });
 })();
